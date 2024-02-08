14172b95b14dc81eed2eada0293b67a07e807bc1
Avinash <avkr258@gmail.com>
2024-02-08 14:20:27 -0500

    Returned temperature for any station and date

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask,render_template
+import pandas as zx
+
 
 web = Flask(__name__)
 
@@ -10,7 +12,9 @@ def home():
 
 @web.route("/ap1/ver1/<stn>/<date>")
 def about(stn, date):
-    temp = 23
+    data = zx.read_csv("data_small/TG_STAID" + str(stn).zfill(6) + ".txt", skiprows=20,
+                       parse_dates=["    DATE"])
+    temp = data.loc[data['    DATE'] == date]['   TG'].squeeze() / 10
     return {"Station": stn,
             "Date": date,
             "Temperature": temp}
